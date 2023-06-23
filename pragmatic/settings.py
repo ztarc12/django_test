a36ede27f97da8c39517e839bc3a7c0194e0f7ca
LimJAEHO <ztarc12@gmail.com>
2023-06-23 10:06:44 +0900

    django course 11 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -128,7 +128,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
